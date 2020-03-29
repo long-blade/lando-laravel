23701c3a8e1e02503b5e53069d84b515df7a06d0
michael <michael@elearningindustry.com>
2020-03-29 22:54:08 +0300

    Remove echo from composer-setup.sh

--- tooling/composer-setup.sh
+++ tooling/composer-setup.sh
@@ -5,16 +5,9 @@ HAS_COMPOSER=$?;
 
 if [ "$HAS_COMPOSER" != "0" ]
 then
-    echo 'Getting composer...!';
     curl -s http://getcomposer.org/installer | php;
-    echo 'Composer self update ..';
     php composer.phar self-update;
-    echo 'Composer self-update is done!';
-    echo 'Move composer.phar to /usr/local/bin/compose';
     mv composer.phar /usr/local/bin/composer;
-    echo 'Composer require hirak/prestissimo!';
-    composer global require hirak/prestissimo --profile;
-    echo 'Composer require laravel/installer!';
-    composer global require laravel/installer --profile;
-    echo 'Composer setup is done!';
+    composer global require hirak/prestissimo;
+    composer global require laravel/installer;
 fi
